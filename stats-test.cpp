1dcc10f0573b83cd599e11c97947242451fb0b18
anupriya0601 <40851096+anupriya0601@users.noreply.github.com>
2022-05-18 18:45:20 +0530

    Update stats-test.cpp
    resolve test file errors.

--- stats-test.cpp
+++ stats-test.cpp
@@ -17,10 +17,12 @@ TEST_CASE("reports average, minimum and maximum") {
 }
 
 TEST_CASE("average is NaN for empty array") {
-    Stats computedStats = compute_statistics(0, 0);
+    struct Stats computedStats = compute_statistics(0, 0);
     //All fields of computedStats (average, max, min) must be
     //NAN (not-a-number), as defined in math.h
-    
+    REQUIRE(computedStats.average != computedStats.average);
+    REQUIRE(computedStats.max != computedStats.max);
+    REQUIRE(computedStats.min != computedStats.min);
     //Design the REQUIRE statement here.
     //Use https://stackoverflow.com/questions/1923837/how-to-use-nan-and-inf-in-c
 }
@@ -28,11 +30,13 @@ TEST_CASE("average is NaN for empty array") {
 TEST_CASE("raises alerts when max is greater than threshold") {
     // create additional .c and .h files
     // containing the emailAlerter, ledAlerter functions
+    int emailAlerter = 2;
+    int ledAlerter = 3;
     alerter_funcptr alerters[] = {emailAlerter, ledAlerter};
 
     float numberset[] = {99.8, 34.2, 4.5};
     int setlength = sizeof(numberset) / sizeof(numberset[0]);
-    Stats computedStats = compute_statistics(numberset, setlength);
+    struct Stats computedStats = compute_statistics(numberset, setlength);
 
     const float maxThreshold = 10.2;
     check_and_alert(maxThreshold, alerters, computedStats);
