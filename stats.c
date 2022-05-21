95704c38507fd5fa778707f939dd3cd620aeb1ff
Manuja Anupriya (SX/EDC3) <nuc1cob@bosch.com>
2022-05-21 16:05:00 +0530

    resolve seg fault issue

--- stats.c
+++ stats.c
@@ -13,32 +13,35 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     s.average = sum / setlength;
     
     /*If there is only one element then return it as min and max both*/
-    if (setlength == 1)
-  {
-     s.max = numberset[0];
-     s.min = numberset[0];    
-  }  
-      /* If there are more than one elements, then initialize min
-      and max*/
-    if (numberset[0] > numberset[1]) 
-  {
-      s.max = numberset[0];
-      s.min = numberset[1];
-  } 
-  else
-  {
-      s.max = numberset[1];
-      s.min = numberset[0];
-  }  
-    for (i = 2; i<setlength; i++)
-  {
-    if (numberset[i] >  s.max)     
-      s.max = numberset[i];
-   
-    else if (numberset[i] <  s.min)     
-      s.min = numberset[i];
-  }
-    return s;
+    if(setlength > 0)
+    {
+      if (setlength == 1)
+      {
+        s.max = numberset[0];
+        s.min = numberset[0];    
+      }  
+          /* If there are more than one elements, then initialize min
+          and max*/
+      if (numberset[0] > numberset[1]) 
+      {
+          s.max = numberset[0];
+          s.min = numberset[1];
+      } 
+      else
+      {
+          s.max = numberset[1];
+          s.min = numberset[0];
+      }  
+      for (i = 2; i<setlength; i++)
+      {
+        if (numberset[i] >  s.max)     
+          s.max = numberset[i];
+      
+        else if (numberset[i] <  s.min)     
+          s.min = numberset[i];
+      }
+    }    
+  return s;
 }
 
 int emailAlertCallCount = 0;
